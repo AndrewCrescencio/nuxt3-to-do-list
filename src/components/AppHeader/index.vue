1634f6bc2cc9ff2f97af0fe306eec0126ad25140
Gabriel Ancelloni <andrew_workstation@hotmail.com>
2024-10-15 04:48:35 -0300

    fix: app-header border bottom

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -1,7 +1,11 @@
 <template>
   <header class="!fixed h-[64px] top-0 left-0 w-screen">
-    <el-menu class="container !border-b-none !mx-auto flex justify-between" mode="horizontal" router
-      style="background-color: var(--ep-bg-color) !important; z-index: 12">
+    <el-menu
+      class="container !mx-auto flex justify-between"
+      mode="horizontal"
+      router
+      style="background-color: var(--ep-bg-color) !important; z-index: 12"
+    >
       <el-menu-item index="/">Home</el-menu-item>
       <client-only>
         <el-menu-item>
