aa66a282e302c17e56af13b25e3eb5f63bd53ec0
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-15 08:10:01 -0300

    feat: initial structure of the TodoItem component

---
+++ src/components/TodoItem/index.story.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import type { Todo } from "~/types/todo.type";
+
+const todo = reactive<Todo>({
+  description: "Uma descrição para a tarefa",
+  id: 1,
+  title: "Fazer coisas legais",
+  user_id: "223aad",
+  createdAt: "",
+  status: true,
+});
+</script>
+
+<template>
+  <Story title="TodoItem component">
+    <TodoItem :todo="todo" />
+  </Story>
+</template>
